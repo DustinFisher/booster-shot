f7ee20650770afda6f666ca173d05ce399b5942f
Dustin Fisher <dustin@dustinfisher.com>
2019-01-31 13:02:47 -0500

    adding option for tailwindcss

--- template.rb
+++ template.rb
@@ -1,20 +1,105 @@
 def go_go_template!
+  add_template_repository_to_source_path
+
+  # default install variables
+  @tailwindcss = true
+
+  gather_user_input
+  add_gems
+
   after_bundle do
+    install_webpack
+    install_tailwindcss
+
     create_and_migrage_db
     initial_project_commit_and_branch
   end
 end
 
+def gather_user_input
+  return if no?('Would you like to customize any of the setup?')
+
+  add_tailwindcss
+end
+
+def add_tailwindcss
+  @tailwindcss = yes?('Would you like to add TailwindCSS?') ? @tailwindcss : nil
+end
+
+def add_tailwindcss?
+  @tailwindcss.present?
+end
+
+def add_webpack?
+  add_tailwindcss?
+end
+
+def add_gems
+  gem 'webpacker' if add_webpack?
+end
+
+def install_webpack
+  return unless add_webpack?
+
+  rails_command 'webpacker:install'
+end
+
+def install_tailwindcss
+  return unless add_tailwindcss?
+
+  run 'yarn add tailwindcss --dev'
+  run './node_modules/.bin/tailwind init app/javascript/css/tailwind.js'
+  copy_file 'app/javascript/css/tailwind.css'
+  insert_into_file 'app/views/layouts/application.html.erb',
+                   before: "</head>\n" do
+    <<-RUBY
+  <%= javascript_pack_tag 'application' %>
+    <%= stylesheet_pack_tag 'application' %>
+    RUBY
+  end
+
+  insert_into_file 'app/javascript/packs/application.js',
+                   before: "console.log('Hello World from Webpacker')\n" do
+    <<-RUBY
+import "../css/tailwind.css";
+    RUBY
+  end
+
+  gsub_file "app/javascript/packs/application.js",
+            /console.log\('Hello World from Webpacker'\)\n/, ''
+
+  insert_into_file '.postcssrc.yml',
+                   after: "postcss-import: {}\n" do
+    <<-RUBY
+  tailwindcss: './app/javascript/css/tailwind.js'
+    RUBY
+  end
+end
+
 def create_and_migrage_db
-  rake "db:create"
-  rake "db:migrate"
+  rake 'db:create'
+  rake 'db:migrate'
 end
 
 def initial_project_commit_and_branch
   git :init
-  git :add => "-A ."
-  git :commit => "-n -m 'Intializing a new project'"
-  git :checkout => "-b development"
+  git add: "-A ."
+  git commit: "-n -m 'Intializing a new project'"
+  git checkout: "-b development"
+end
+
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    source_paths.unshift(tempdir = Dir.mktmpdir('booster-shot-'))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      '--quiet',
+      'https://github.com/dustinfisher/booster-shot.git',
+      tempdir
+    ].map(&:shellescape).join(' ')
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
 end
 
 go_go_template!
