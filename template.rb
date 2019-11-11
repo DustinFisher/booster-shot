9672ed9f13fd99615dff6d2b8745f75fde0e01f9
Dustin Fisher <dustin@dustinfisher.com>
2019-11-11 10:03:38 -0500

    remove default style and js engine

--- template.rb
+++ template.rb
@@ -7,20 +7,17 @@ def go_go_template!
   @rspec              = true
   @tailwindcss        = true
   @devise             = true
-  @js_framework       = 'react'
   @omniauth           = true
   @omniauth_twitter   = true
   @omniauth_google    = true
   @omniauth_facebook  = true
   @action_text        = true
   @mobile_friendly    = true
-  @default_style      = true
+  @js_framework       = 'react'
 
   gather_user_input
 
   add_gems
-
-  use_js_not_coffee
 
   after_bundle do
     install_rspec
@@ -30,7 +27,6 @@ def go_go_template!
     install_js_framework
     install_action_text
     install_mobile_friendly_tag
-    # install_default_style
 
     create_db
     migrate_db
@@ -56,7 +52,6 @@ def gather_user_input
   input_add_omniauth
   input_add_action_text
   input_add_mobile_friendly_tag
-  # input_add_default_style
 end
 
 def input_add_devise
@@ -82,10 +77,6 @@ end
 
 def input_add_action_text
   @action_text = yes?('Would you like to add Action Text?') ? @action_text : false
-end
-
-def input_add_default_style
-  @default_style = yes?('Would you like to add a default style to the app?') ? @default_style : false
 end
 
 def input_add_mobile_friendly_tag
@@ -114,15 +105,6 @@ end
 
 def add_mobile_friendly_tag?
   @mobile_friendly
-end
-
-def use_js_not_coffee
-  inject_into_file 'config/application.rb',
-    after: "config.load_defaults 5.2\n" do <<-RUBY
-
-    config.generators.javascript_engine = :js
-    RUBY
-  end
 end
 
 def create_db
